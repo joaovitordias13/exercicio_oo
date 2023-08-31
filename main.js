94f082b3a94485d2bc7f29dc04cb04fdc0547faa
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-08-31 20:20:48 -0300

    Feito

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-function Marvel(nome) {
+function Hero(nome) {
     this.nome = nome;
 }
-function Hero(nome,poderes,idade) {
-    Marvel.call(this,nome);
+
+function Heros(nome,poderes,idade,editora) {
+    Hero.call(this,nome);
 
     this.poderes = poderes
     this.idade = idade 
+    this.editora = editora
 
 
 }
 
-const hero1= new Hero('Capitão America', "super força, super resistencia", 102);
-const hero2= new Funcionario('Maria', "dev front-end", 5000);
-const hero3= new Funcionario('Maria', "dev front-end", 5000);
+const hero1= new Heros('Capitão America', "super força, super resistencia,sentidos sobre-humanos", 102,"Marvel");
+const hero2= new Heros('Batmam', "Ter dinheiro",45, "DC");
+const hero3= new Heros('Homem-Aranha', "super força,agilidade,lançar teias,sentidos sobre-humanos;capacidade de escalar paredes;", 20,"Marvel");
+
+
+console.log(hero1);
+console.log(hero2);
+console.log(hero3);
